ea763de5d665f2ce5cbb921b372bfbe71fe97aa7
shravan-kaja <shravan.kaja@gmail.com>
2021-02-08 15:08:04 +0530

    Added: Equality checking code

--- src/LineComputationComparison.java
+++ src/LineComputationComparison.java
@@ -19,6 +19,29 @@ public class LineComputationComparison {
 		double P= Math.pow(A, 2) + Math.pow(B, 2);
 		double R=Math.sqrt(P);
 		System.out.println("Lenght of the line based on the coordinates is : "+R);
+		
+		System.out.println("Enter Point of second line (x1,y1)");
+		int x11= sc.nextInt();
+		int y11= sc.nextInt();  
+	
+		System.out.println("Enter Point (x2,y2)");
+		int x22= sc.nextInt();
+		int y22= sc.nextInt(); 
+		
+		int A1=x22-x11;
+		int B1=y22-y11;
+		double P1= Math.pow(A1, 2) + Math.pow(B1, 2);
+		double R1=Math.sqrt(P1);
+		System.out.println("Lenght of the second line based on the coordinates is : "+R1);
+		if(R==R1)
+		{
+			System.out.println("Lines are equal");
+		}
+		else
+		{
+			System.out.println("Not equal");
+		}
+	
 	}
 
 }
