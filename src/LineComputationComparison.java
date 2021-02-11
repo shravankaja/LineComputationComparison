be21224bc1f0a5cdcca983aee8f03b408e14924c
shravan-kaja <shravan.kaja@gmail.com>
2021-02-11 13:44:01 +0530

    Refactored: Added constructor, class and object

--- src/LineComputationComparison.java
+++ src/LineComputationComparison.java
@@ -5,28 +5,11 @@ public class LineComputationComparison {
 	public static void main(String Args[])
 	{
 		System.out.println("Welcome to Line Computation and Comparison Program");
-		Main m=new Main();
-		System.out.println("Enter Point (x1,y1)");
-		Scanner sc= new Scanner(System.in);
-		float x1= sc.nextFloat();
-		float y1= sc.nextFloat();  
-	
-		System.out.println("Enter Point (x2,y2)");
-		float x2= sc.nextFloat();
-		float y2= sc.nextFloat(); 
-		double resultLine1=m.computeLine(x1,y1,x2,y2);
+		Main objOne=new Main(4,8,9,4,2,6,3,8);
+		objOne.computeLine();
+		objOne.compareTo();
+		objOne.equalsTo();
 		
-		System.out.println("Enter Point of second line (x1,y1)");
-		float x11= sc.nextFloat();
-		float y11= sc.nextFloat();  
-	
-		System.out.println("Enter Point (x2,y2)");
-		float x22= sc.nextFloat();
-		float y22= sc.nextFloat();
-		double resultLine2 = m.computeLine(x11,y11,x22,y22);
-		
-		m.equalsTo(resultLine1,resultLine2);
-		m.compareTo(resultLine1,resultLine2);
 	}
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,71 @@
 
 public class Main {
-
-	double computeLine(float x, float y, float z, float k)
-	{
-		float A=x-y;
-		float B=z-k;
-		double Power = Math.pow(A, 2) + Math.pow(B, 2);
-		double resultLine1 = Math.sqrt(Power);
-		System.out.println("Lenght of the line  based on the coordinates is : "+resultLine1);
-		return resultLine1;
-	}
-	static void equalsTo(double x, double y) 
-	{
-			if(x==y)
-					{
-						System.out.println("Lines are equal");
-					}
-			else 
-					{
-						System.out.println("Lines are not equal");
-					}
-	}
-static void compareTo(double x, double y)
-	{
-			if (x > y)
-					{
-						System.out.println("Line1 is greater than Line2");
-					}
-			else if (y > x)
-					{
-						System.out.println("Line2 is greater than Line1");
-					}
 	
-	}
+	
+	private final float POINT_X1;
+	private final float POINT_Y1;
+	private final float POINT_X2;
+	private final float POINT_Y2;
+	private final float POINT_X12;
+	private final float POINT_Y12;
+	private final float POINT_X22;
+	private final float POINT_Y22;
+	public double resultLineOne;
+	public  double resultLineTwo;
+	
+
+	public Main(float p_x1, float p_y1, float p_x2, float p_y2, float p_x12, float p_y12, float p_x22, float p_y22)
+			{
+				this.POINT_X1 = p_x1;
+				this.POINT_Y1 = p_y1;
+				this.POINT_X2 = p_x2;
+				this.POINT_Y2 = p_y2;
+				this.POINT_X12 = p_x12;
+				this.POINT_Y12 = p_y12;
+				this.POINT_X22 = p_x22;
+				this.POINT_Y22 = p_y22;
+			}
+	
+	
+
+	void computeLine()
+				{
+					float temp1 =  POINT_X1 - POINT_X2;
+					float temp2 =  POINT_Y2 - POINT_Y1;
+					float temp3 =  POINT_X22 - POINT_X12;
+					float temp4 =  POINT_Y22 - POINT_Y12;
+			
+					double Power = Math.pow(temp1, 2) + Math.pow(temp2, 2);
+					resultLineOne = Math.sqrt(Power);
+					System.out.println("Lenght of the line  based on the coordinates is : "+resultLineOne);
+					double Power1 = Math.pow(temp3, 2) + Math.pow(temp4, 2);
+					resultLineTwo = Math.sqrt(Power1);
+					System.out.println("Lenght of the line  based on the coordinates is : "+resultLineTwo);
+				}
+	 
+	void equalsTo() 
+	 			{
+					if(this.resultLineOne == this.resultLineTwo)
+							{
+								System.out.println("Lines are equal");
+							}
+					else 
+							{
+								System.out.println("Lines are not equal");
+							}
+	 			}
+	void compareTo()
+				{
+					if (this.resultLineOne>this.resultLineTwo)
+							{
+								System.out.println("Line1 is greater than Line2");
+							}
+					else if (resultLineOne<resultLineTwo)
+							{
+								System.out.println("Line2 is greater than Line1");
+							}
+	
+				}
 	
 	
 }
