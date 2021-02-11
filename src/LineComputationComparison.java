f44fc87ac811cb9f9e14f8f8fff32c767b9947a4
shravan-kaja <shravan.kaja@gmail.com>
2021-02-11 17:13:23 +0530

    Refactored: Built two seperarte classes for each line

--- src/LineComputationComparison.java
+++ src/LineComputationComparison.java
@@ -1,19 +1,15 @@
 import java.util.*;
-import java.lang.*; 
+import java.lang.*;
+
 public class LineComputationComparison {
-	
-	public static void main(String Args[])
-	{
+
+	public static void main(String Args[]) {
 		System.out.println("Welcome to Line Computation and Comparison Program");
-		Main objOne=new Main(4,8,9,4,2,6,3,8);
-		objOne.computeLine();
-		objOne.compareTo();
-		objOne.equalsTo();
-		
+		Main line1_Lenght = new Main(4.1,8.5,9.1,4.7);
+		Main line2_Lenght = new Main(5.7,6.8,9.3,8.5);
+		double lenght_Line1 = line1_Lenght.computeLine();
+		double lenght_Line2 = line2_Lenght.computeLine();
+		Main.compareTo(lenght_Line1, lenght_Line2);
 	}
 
 }
-
-
-	
- 
--- src/Main.java
+++ src/Main.java
@@ -1,71 +1,37 @@
 
 public class Main {
-	
-	
-	private final float POINT_X1;
-	private final float POINT_Y1;
-	private final float POINT_X2;
-	private final float POINT_Y2;
-	private final float POINT_X12;
-	private final float POINT_Y12;
-	private final float POINT_X22;
-	private final float POINT_Y22;
-	public double resultLineOne;
-	public  double resultLineTwo;
-	
 
-	public Main(float p_x1, float p_y1, float p_x2, float p_y2, float p_x12, float p_y12, float p_x22, float p_y22)
-			{
-				this.POINT_X1 = p_x1;
-				this.POINT_Y1 = p_y1;
-				this.POINT_X2 = p_x2;
-				this.POINT_Y2 = p_y2;
-				this.POINT_X12 = p_x12;
-				this.POINT_Y12 = p_y12;
-				this.POINT_X22 = p_x22;
-				this.POINT_Y22 = p_y22;
-			}
-	
-	
+	private final double POINT_X1;
+	private final double POINT_Y1;
+	private final double POINT_X2;
+	private final double POINT_Y2;
+	public double result_Line_Lenght;
 
-	void computeLine()
-				{
-					float temp1 =  POINT_X1 - POINT_X2;
-					float temp2 =  POINT_Y2 - POINT_Y1;
-					float temp3 =  POINT_X22 - POINT_X12;
-					float temp4 =  POINT_Y22 - POINT_Y12;
-			
-					double Power = Math.pow(temp1, 2) + Math.pow(temp2, 2);
-					resultLineOne = Math.sqrt(Power);
-					System.out.println("Lenght of the line  based on the coordinates is : "+resultLineOne);
-					double Power1 = Math.pow(temp3, 2) + Math.pow(temp4, 2);
-					resultLineTwo = Math.sqrt(Power1);
-					System.out.println("Lenght of the line  based on the coordinates is : "+resultLineTwo);
-				}
-	 
-	void equalsTo() 
-	 			{
-					if(this.resultLineOne == this.resultLineTwo)
-							{
-								System.out.println("Lines are equal");
-							}
-					else 
-							{
-								System.out.println("Lines are not equal");
-							}
-	 			}
-	void compareTo()
-				{
-					if (this.resultLineOne>this.resultLineTwo)
-							{
-								System.out.println("Line1 is greater than Line2");
-							}
-					else if (resultLineOne<resultLineTwo)
-							{
-								System.out.println("Line2 is greater than Line1");
-							}
-	
-				}
-	
-	
+	public Main(double p_x1, double p_y1, double p_x2, double p_y2) {
+		this.POINT_X1 = p_x1;
+		this.POINT_Y1 = p_y1;
+		this.POINT_X2 = p_x2;
+		this.POINT_Y2 = p_y2;
+	}
+
+	double computeLine() {
+		double temp1 = POINT_X1 - POINT_X2;
+		double temp2 = POINT_Y2 - POINT_Y1;
+
+		double Power = Math.pow(temp1, 2) + Math.pow(temp2, 2);
+		result_Line_Lenght = Math.sqrt(Power);
+		System.out.println("Lenght of the line  based on the coordinates is : " + result_Line_Lenght);
+		return result_Line_Lenght;
+	}
+
+	static void compareTo(double len_Line1, double len_Line2) {
+		if (len_Line1 > len_Line2) {
+			System.out.println("Line1 is greater than Line2");
+		} else if (len_Line1 == len_Line2) {
+			System.out.println("Lines are equal");
+		} else {
+			System.out.println("Line 2 is grater than Line1");
+		}
+	}
+
 }
